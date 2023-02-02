603bd9c1b7e649bf0ab4f14d3a35f5aad7cb0bdf
Vibhuti Bajaj <vibhutibajaj2000@gmail.com>
2023-02-02 13:41:22 +0530

    Added innertext for all elements

--- src/component/CardComponent.js
+++ src/component/CardComponent.js
@@ -1,9 +1,12 @@
 class CardComponent{
     constructor(individualData){
         this.individualData = individualData;
+        this.isMounted = false;
     }
     render(){
         //Creating the component
+        const {name, profileImage, introduction, profileLink} = this.individualData;
+
         const card = document.createElement('div');
         const cardHeader = document.createElement('div');
         const cardImageDiv = document.createElement('div');
@@ -20,6 +23,21 @@ class CardComponent{
         cardContentDiv.classList.add("card-content");
         cardLink.classList.add("card-link");
 
+        //adding innerText
+        // introduction
+        //name
+        //profileImage
+        //profileLink
+        
+        cardTitle.innerText = name;
+        cardContent.innerText = introduction;
+        cardLink.href = profileLink;
+        cardLink.innerText = "Know more";
+        cardLink.target = "_blank";
+        cardImage.src = profileImage;
+        cardImage.alt = name;
+
+
         // appendchild 
 
         card.appendChild(cardHeader);
@@ -32,5 +50,16 @@ class CardComponent{
         
         return card;
 
+    }
+    mount(el){
+        if (this.isMounted) return
+        if(el){
+            el.appendChild(this.render());
+            this.isMounted = true;
+            return;
+        }
+        document.body.appendChild(this.render());
+        this.isMounted = true;
+        return;
     }
 }
