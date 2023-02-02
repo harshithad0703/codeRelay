5136b6fc3b7ea28c4c0cee21fba4c77a6241283e
raj pandey <raj.pandey@contentstack.com>
2023-02-02 13:07:36 +0530

    Written a part of render component

---
+++ src/component/CardComponent.js
@@ -0,0 +1,19 @@
+class CardComponent{
+    constructor(individualData){
+        this.individualData = individualData;
+    }
+    render(){
+        //Creating the component
+        const card = document.createElement('div');
+        const cardHeader = document.createElement('div');
+        const cardImageDiv = document.createElement('div');
+        const cardImage = document.createElement('img');
+        const cardTitle = document.createElement('h3');
+        const cardContentDiv = document.createElement('div');
+        const cardContent = document.createElement('p');
+        const cardLink = document.createElement('a');
+
+        //Assiging the classes and ID's
+
+    }
+}
